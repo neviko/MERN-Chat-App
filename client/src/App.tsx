92a52a43055c1132ba903469a6976c97af8b0f15
neviko <nevosayag@gmail.com>
2023-08-03 08:59:41 +0300

    fetching groups

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { io, Socket } from "socket.io-client";
 import { Eevents } from "./common/enums/events";
 import ChatRegistration from "./components/ChatRegistration";
+import { UseFetchData } from "./components/UseFetchData";
+import { BASE_URL } from "./constants/url";
+import axios from "axios";
 
 let socket;
+const url = `${BASE_URL}/api/group`;
 
 function App() {
+  const [nickname, setNickname] = useState<string>();
+  const [groups, setGroups] = useState<string[]>();
+
   useEffect(() => {
     try {
-      const socket = io("http://localhost:5000");
+      socket = io("http://localhost:5000");
+      console.log("socket connected");
     } catch (e) {
-      console.error("weeor while connecting to websocket", e);
+      console.error("error while connecting to websocket", e);
     }
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        if (!nickname) {
+          return;
+        }
+        const { data } = await axios.get<string[]>(url);
+        console.log(data);
+        if (data) {
+          setGroups(data as string[]);
+        }
+      } catch (e) {
+        console.log("error while fetching data");
+      }
+    })();
+  }, [nickname]);
+
   return (
     <div className="App">
-      nevo
-      <ChatRegistration />
+      <h1>WeMatch Chat App</h1>
+      {!nickname && (
+        <ChatRegistration
+          onNicknameSet={(nickName: string) => setNickname(nickName)}
+        />
+      )}
     </div>
   );
 }
--- client/src/components/ChatRegistration.tsx
+++ client/src/components/ChatRegistration.tsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
-import { UseFetchData } from "./UseFetchData";
 
-const ChatRegistration = () => {
+interface IProps {
+  onNicknameSet: (nickname: string) => void;
+}
+
+const ChatRegistration: React.FC<IProps> = ({ onNicknameSet }) => {
   const [nickName, setNickName] = useState<string>("");
-
-  const handleSubmit = (e: React.FormEvent<SubmitEvent>) => {
-    // input validation here
-    const url = "localhost:5000/api/groups/";
-    const {
-      data: getData,
-      loading: getLoading,
-      error: getError,
-    } = UseFetchData({ url });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // TODO: input validation here
+    e.preventDefault();
+    onNicknameSet(nickName);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}></form>
-      <TextField
-        id="standard-basic"
-        label="Email or Nickname"
-        variant="standard"
-        onChange={(e) => setNickName(e.target.value)}
-      />
-      <Button type="submit" />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="standard-basic"
+          label="Email or Nickname"
+          variant="standard"
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <Button type="submit">Login</Button>
+      </form>
     </div>
   );
 };
